e256f3f1683aeee01ccf97c3855c8ac754721256
WanderingStar <github@account.loath.org>
2022-11-11 17:53:36 -0600

    added birdnet

---
+++ birdnet.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+from datetime import datetime, timezone
+import re
+import mechanicalsoup
+
+from config import config
+from core import database, export
+from helpers import logger
+
+# Initialize the logger
+logger = logger.get_logger('gps_exporter')
+
+if __name__ == '__main__':
+    where = "utc_time IS NOT NULL ORDER BY utc_time DESC LIMIT 1"
+
+    # Initialization
+    config_file = "./config/config.json"
+
+    # Read the application config
+    appConfig = config.AppConfig(config_file)
+    rc = appConfig.load_app_config()
+
+    try:
+        # attempt to connect to database (create database if does not already exist)
+        connection_handler = database.connect(db_filename=appConfig.database_filename)
+        # if no connection handler, then give up
+        if connection_handler is not None:
+            data = database.retrieve_data_where(connection_handler, where)
+            if data[0]:
+                lat = round(data[0].latitude, ndigits=4)
+                lon = round(data[0].longitude, ndigits=4)
+                browser = mechanicalsoup.StatefulBrowser(user_agent='MechanicalSoup')
+                response = browser.open("http://localhost/views.php?view=Settings", auth=("birdnet", ""))
+                if not response.ok:
+                    print(f"Got {response.status_code} from {browser.get_url()}")
+                    exit(1)
+                form = browser.select_form("#basicform")
+                form["latitude"] = lat
+                form["longitude"] = lon
+                response = browser.submit_selected(auth=("birdnet", ""))
+                if not response.ok:
+                    print(f"Got {response.status_code} from {browser.get_url()}")
+                    exit(1)
+        #print(f"Set lat, lon to ({lat}, {lon})")
+
+    except Exception as error:
+        logger.error(f"Exception: {str(error)}")
+
